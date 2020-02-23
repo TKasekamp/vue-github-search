b32572d5220175943335c6838c6fe4a0f1bcbef2
Tõnis Kasekamp <toniskasekamp@gmail.com>
2020-02-23 11:25:05 +0200

    Add composition API experimentation

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <composition/>
   </div>
 </template>
+<script>
+// @ is an alias to /src
+import Composition from '@/components/Composition.vue';
+
+export default {
+  name: 'About',
+  components: {
+    Composition,
+  },
+};
+</script>
